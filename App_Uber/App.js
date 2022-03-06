1e618022d001757489b16cd9af6ea3bcac03f177
Angel Quintanilla <angeldavidquintanilla@gmail.com>
2022-03-06 15:51:49 -0600

    Arreglos View Viajes

--- App_Uber/App.js
+++ App_Uber/App.js
@@ -1,4 +1,6 @@
 import Viajes from './src/componentes/viewViajes';
+import Marcas from './src/componentes/viewMarcas';
+import Modelos from './src/componentes/viewModelos';
 export default function App() {
   return (
     <Viajes></Viajes>
--- App_Uber/src/componentes/viewViajes.js
+++ App_Uber/src/componentes/viewViajes.js
@@ -26,7 +26,9 @@ const Viajes = () => {
         <TouchableOpacity style={styles.button} onPress={() => Alert.alert("Registro Guardado Correctamente")}>
           <Text style={styles.buttonText}>REGISTRAR</Text>
         </TouchableOpacity>
-
+        <TouchableOpacity style={styles.buttonCancelar} onPress={() => Alert.alert("Registro Guardado Correctamente")}>
+          <Text style={styles.buttonText}>CANCELAR</Text>
+        </TouchableOpacity>
       </View>
 
     </ImageBackground>
@@ -35,7 +37,7 @@ const Viajes = () => {
 
 
 const styles = StyleSheet.create({
-  container: { flex: 1, fontFamily: 'Inter_900Black' },
+  container: { flex: 1, fontFamily:'Times New Roman',},
   image: {
     flex: 1,
     padding: 10,
@@ -59,9 +61,12 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: '#fff',
     textAlign: 'center',
-    fontWeight: 'bold',
+    fontWeight: 'bold', 
   },
-
+  buttonCancelar:{
+    backgroundColor: '#B51F00', padding: 10, borderRadius: 15, marginTop: 15,
+    
+  },
   button: {
     backgroundColor: '#0072E9', padding: 10, borderRadius: 15, marginTop: 15,
   },
