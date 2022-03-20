947dabe8c00a2043a8aa6544bfeeba8176ab2b4e
Luis Flores <lf016158@gmail.com>
2022-03-20 09:58:06 -0600

    se creo la pantalla para viajes

--- App_Uber/src/Slices/navSlice.js
+++ App_Uber/src/Slices/navSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
     origin: null,
     destination: null,
+    vehiculos:null,
     travelTimeInformation: null
 }
 
@@ -18,14 +19,17 @@ export const navSlice = createSlice({
         },
         setTravelTimeInformation: (state, action) => {
             state.travelTimeInformation = action.payload;
-        }
+        },
+        setVehiculos: (state, action) => {
+            state.vehiculos = action.payload;
+        },
     },
 });
 
-export const { setOrigin, setDestination, setTravelTimeInformation } = navSlice.actions;
+export const { setOrigin, setDestination, setTravelTimeInformation,setVehiculos } = navSlice.actions;
 
 export const selectedOrigin = (state) => state.nav.origin;
 export const selectedDestination = (state) => state.nav.destination;
 export const selectedTravelTimeInformation = (state) => state.nav.travelTimeInformation;
-
+export const selectedVehiculos = (state) => state.nav.vehiculos;
 export default navSlice.reducer;
